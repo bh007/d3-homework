d91fa5bd7ea577ef155dedb7070bbda50ebff592
Yawei Hui <ywhui30@gmail.com>
2016-09-28 23:07:52 -0400

    Migrate to VS code IDE

---
+++ scatterplot/app.js
@@ -0,0 +1,12 @@
+/// <reference path="typings/node/node.d.ts" />
+/// <reference path="typings/express/express.d.ts" />
+/// <reference path="typings/body-parser/body-parser.d.ts" />
+/// <reference path="typings/d3/d3.d.ts" />
+
+var express = require('express');
+var app = express();
+app.use('/', express.static('./'));
+app.get('/', function(req, res){
+    res.sendFile('index.html', { root: __dirname });
+});
+app.listen(45000);
